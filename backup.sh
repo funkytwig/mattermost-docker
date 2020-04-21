537b84051d10ea2ac778deccb59643deeca217da
Ben Edwards <ben@funkytwig.com>
2020-04-21 02:32:40 +0100

    touch file to set date and, create _D_ file and delete olf _F_ files

--- backup.sh
+++ backup.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash 
 set -u # or set -o nounset
 
 . .env # put configuration varable in here
 
 date=`date "+%a_%d%b%Y_%H%M"`
 seq=`date +%s` # julian seconds
+datetime=`date "+%Y%m%d%H%M"`
 
 backup_dir=$backup_path/${backup_name}_${seq}_F_${date}
 new_backup=$backup_login:$backup_dir
@@ -42,6 +43,7 @@ fi
 echo
 
 if [ $? -eq 0 ]; then
+  ssh $backup_login "touch -t $datetime $backup_dir"
   ssh $backup_login "rm $latest_backup"
   ssh $backup_login "ln -s $backup_dir $latest_backup"
   echo "Backup worked :-)"
@@ -50,5 +52,31 @@ else # failed so bacup probably only partial
   ssh $backup_login "rm -rf $backup_dir"
   echo "Backup FAILED :-( - deleted $backup_dir"
 fi
+
+# Rename last _F_ to _D_ and delete other _F_
+
+# set cut for DDMonYYYY
+cut_from=${#backup_name}
+let cut_from=$cut_from+1
+let cut_to=$cut_from+8
+
+# Get list of days (DDMonYYYY) which have files older then 2 days
+days=`ssh $backup_login \
+  "find $backup_path -name \"*_F_*\" -maxdepth 1 -mtime +2|rev|cut -c$cut_from-$cut_to|rev|sort|uniq"`
+
+del_list=`ssh $backup_login \
+  "find $backup_path -name \"*_F_*\" -maxdepth 1 -mtime +2|sort"`
+
+for day in $days; do
+  oldest_file=`ssh $backup_login "ls -td ${backup_path}/*${day}*|sort|tail -1"`
+  rename_to=`echo $oldest_file| sed "s/_F_/_D_/g"`
+  echo "Rename $oldest_file to $rename_to"
+  ssh $backup_login  "mv $oldest_file $rename_to"
+done
+
+for file in $del_list; do
+  echo "rm $file"
+  ssh $backup_login "rm -rf $file"
+done
 
 # sudo -u ben bash -c "docker-compose up -d"
